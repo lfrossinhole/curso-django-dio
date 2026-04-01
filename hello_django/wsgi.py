572d6b6453a102ee519ac51b113caca8993d38a7
Lucas Feitosa Rossinhole <lucas.rossinhole@hotmail.com>
2026-04-01 17:14:07 -0300

    criacao de admin e model

--- hello_django/wsgi.py
+++ hello_django/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+# AULA 02 - esgi.py é um servidor para DESENVOLVIMENTO, não é recomendado para produção.
+
 import os
 
 from django.core.wsgi import get_wsgi_application
